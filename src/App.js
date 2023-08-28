4674c2538d7cd018b3bc7e306ee9974de16ff260
Collins Murgor <vintoncollins702@gmail.com>
2023-08-28 11:38:38 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function Clock({ time }) {
+  let hours = time.getHours();
+  let className;
+  if (hours >= 0 && hours <= 6) {
+    className = "night";
+  } else {
+    className = "day";
+  }
+  return <h1 className={className}>{time.toLocaleTimeString()}</h1>;
+}
